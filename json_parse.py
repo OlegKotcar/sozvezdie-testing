ef8992bbfec7621c9884a6b89da140fe491b1787
Oleg Kotsar <jonin.j@gmail.com>
2020-11-02 18:02:40 +0800

    Try JSON parsing

---
+++ json_parse.py
@@ -0,0 +1,243 @@
+#from bs4 import BeautifulSoup
+import json
+import urllib.request, requests, time
+from datetime import datetime
+
+"""
+{
+description:	
+Детальная информация по предложению
+
+id*	integer($int64)
+Идентификатор
+
+title	string
+Название
+
+header	string
+Заголовок
+
+description	string
+Описание
+
+route	[...]
+periodStart*	string($date-time)
+Начало ближайшего периода
+
+periodEnd*	string($date-time)
+Окончание ближайшего периода
+
+minPrice*	number($double)
+Минимальная цена
+
+photoCard	PhotoCard{...}
+photoAlbum	[...]
+prices	[...]
+}
+"""
+
+producturl="http://185.10.185.115:7777/tour/"
+
+
+with urllib.request.urlopen("http://185.10.185.115:7777/api/tour") as url:
+    data = json.loads(url.read().decode())
+#    print(data)
+
+descriptons = []
+
+def find_duplicates(lst, item):
+    return [key for key, x in enumerate(lst) if x == item]
+
+def string_to_date(rawString):  
+    return (datetime.strptime(rawString, "%Y-%m-%dT%H:%M:%S%z"))
+
+
+
+# Заполнили массив описаниями
+for key in range (len(data)):
+    descriptons.append(data[key]["description"])
+
+# Проверяем полное совпадение текста в разных продуктах
+for key in range(len(descriptons)):
+    doublelist = (find_duplicates(descriptons, descriptons[key]))
+    if key in doublelist: # убираем ссылку на самого себя
+        doublelist.remove(key)
+    print (f"Описание для тура {producturl}{key} полностью повторяется в турах с id={doublelist}")
+# id потом можно привязать к конечным url
+
+# Проверим даты
+for key in range (len(data)):
+    nearStart = data[key]["periodStart"] # Ближайшая дата начала в каталоге
+    nearEnd =  data[key]["periodEnd"]  # Ближайшая дата окончания в каталоге
+    minPrice = data[key]["minPrice"]  # Минимальная цена в каталоге
+    if nearStart == None:
+        print(f"Для продукта {producturl}{key} нет ближайшей даты начала в каталоге")
+    elif nearEnd == None:    
+        print(f"Для продукта {producturl}{key} нет ближайшей даты окончания в каталоге")
+    elif minPrice == None:
+        print(f"Для продукта {producturl}{key} минимлаьной цены в каталоге")
+    else:
+    # Если даты есть, работаем с ними  Тут еще UTC надо учитывать
+        dateNow = datetime.now()
+        nearStart = string_to_date(nearStart)
+        nearEnd =  string_to_date(nearEnd)          
+        if nearStart > nearEnd:
+            print(f"Для продукта {producturl}{key} в каталоге ближайшее начало тура {nearStart.date()} позже его окончания {nearEnd.date()}")
+        else:
+            print(f"Для продукта {producturl}{key} с датами начала и конца тура все ОК")          
+        if nearStart.date() > dateNow.date():
+            print(f"Для продукта {producturl}{key} в каталоге ближайшее начало тура {nearStart.date()} уже прошло, сегодня {dateNow.date()}")        
+# Собираем массив дат с карточки чтобы проверить даты на самих карточках и срачвнить их с каталожными
+    
+    startDates = []
+    endDates = []
+    productPrices = []
+    
+    prices = (data[key]["prices"])
+    if prices == None:
+         print(f"Для продукта {producturl}{key} нет предложений по датам и ценам в каталоге")
+    else:
+        for price in prices:
+            startDateInProduct = string_to_date(price.get("start"))
+            endDateInProduct = string_to_date(price.get("end"))
+            priceInProduct = price.get("priceValue")
+            #print(f"{startDateInProduct.date()} ---- {endDateInProduct.date()}")
+            if startDateInProduct > endDateInProduct:
+                print(f"На карточке продукта {producturl}{key} ближайшее начало тура {startDateInProduct.date()} позже его окончания {startDateInProduct.date()}") 
+            if startDateInProduct.date() > dateNow.date():
+                print(f"На карточке продукта {producturl}{key} ближайшее начало тура {startDateInProduct.date()} позже его окончания {startDateInProduct.date()}")            
+            if priceInProduct <= 0 or priceInProduct == None:
+                print(f"На карточке продукта {producturl}{key} не указана или неверно указана стоимость {priceInProduct}")               
+            startDates.append(startDateInProduct.date())
+            endDates.append(endDateInProduct.date())
+            productPrices.append(priceInProduct)
+        #print(f"{startDates}")
+        # Тут проверям ближайшие даты в каталоге и в карточке
+        #print(min(startDates))
+        if  nearStart.date() != (min(startDates)):
+            print(f"Для продукта {producturl}{key} ближайшая дата в каталоге {nearStart.date()} отличается от ближайшей даты на карточке продукта {min(startDates)}")
+        else:
+            print(f"С ближайшими датами на карточке продукта {producturl}{key} все ОК")
+        if minPrice != min(productPrices):
+            print(f"Для продукта {producturl}{key} мин. цена в каталоге {minPrice} отличается от мин. цены на карточке продукта {min(productPrices)}")        
+        else:
+            print(f"Мин. цена в каталоге и на карточке продукта {producturl}{key} все ОК")
+    
+    
+    
+    #startDates = (data[key]["prices"])#.get("start")
+    #endDates = (data[key]["prices"])#.get("end")
+    #print(startDates,endDates)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#---------------------------------------------------------------------------------------
+
+print("--------------------Проверяем превью и фото продуктов----------------------------")
+
+# Проверяем  заргузку thumb и картинок с карточек продуктов
+for key in range (len(data)):
+    print(f"---------------------------------Идет проверка photoCard {key}")
+    if data[key]["photoCard"] == None:
+        print(f"В продукте {producturl}{key} отсутствует превью и картинка продукта")
+    else: 
+        thumburl = (data[key]["photoCard"]).get('thumbnail')
+        photourl = (data[key]["photoCard"]).get('photo')
+        # проверяем доступность url для превью продукта
+        if thumburl == "": 
+            print(f"В продукте {producturl}{key} отсутствует ссылка на превью")
+        else:
+            r = requests.get(thumburl)
+            if r.status_code != 200:
+                print(f"На странице продукта {producturl}{key} неверная ссылка на превью, {thumburl} ")
+
+    # проверяем доступность url для фото продукта
+        if photourl == "": 
+            print(f"в продукте {producturl}{key} отсутствует ссылка на фото")
+        else:
+            r = requests.get(photourl)
+            if r.status_code != 200:
+                print(f"На странице продукта {producturl}{key} неверная ссылка на фото, {photourl} ")              
+
+    # -----------проверяем досутупность url и превью для фотоальбома -------
+    print(f"---------------------------------Идет проверка photoAlbum {key}")
+    
+    photoalbum = data[key]["photoAlbum"]
+    if data[key]["photoAlbum"] == None:
+        print(f"В продукте {producturl}{key} отсутствует фотоальбом")
+    else: 
+        for i in range(len(photoalbum)): # цикл по всем картинкам в фотоальбоме
+            thumburl = (photoalbum[i]).get('thumbnail')
+            photourl = (photoalbum[i]).get('photo')
+            #print(f"Это фоточка с альбома {photourl}")
+            
+            # проверяем доступность url для превью в фотоальбоме продукта
+            if thumburl == "":
+                print(f"В продукте {producturl}{key}, в фотоальбоме отсутствует ссылка на превью")
+            else:
+                r = requests.get(thumburl)
+                if r.status_code != 200:
+                    print(f"В фотоальбоме продукта {producturl}{key} неверная ссылка на превью, {thumburl} ")
+
+            # проверяем доступность url для фото в фотоальбоме продукта
+            if photourl == "":
+                print(f"В продукте {producturl}{key}, в фотоальбоме отсутствует ссылка на фото")
+            else:
+                r = requests.get(photourl)
+                if r.status_code != 200:
+                    print(f"В фотоальбоме продукта {producturl}{key} неверная ссылка на фото, {photourl} ")
+
+
+
+
+
+
+
+
+
+
+
+"""
+    # проверяем доступность url для превью продукта
+    if thumburl == "": 
+        print(f"в продукте {producturl}{key} отсутствует ссылка на превью")
+    else:
+        r = requests.get(thumburl)
+        if r.status_code != 200:
+            print(f"На странице продукта {producturl}{key} неверная ссылка на превью, {thumburl} ")
+
+    # проверяем доступность url для фото продукта
+    if photourl == "": 
+        print(f"в продукте {producturl}{key} отсутствует ссылка на фото")
+    else:
+        r = requests.get(photourl)
+        if r.status_code != 200:
+            print(f"На странице продукта {producturl}{key} неверная ссылка на фото, {photourl} ")     
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
